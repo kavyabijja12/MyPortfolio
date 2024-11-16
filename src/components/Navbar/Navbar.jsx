f7f60d4c40c2d51fdf763701577c329aac240b2e
kavyabijja12 <bijjakavya1999@gmail.com>
2024-11-16 14:25:54 -0700

    nav bar responsive done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,32 @@
 import React from 'react'
 import styles from "./Navbar.module.css"
+import { useState } from 'react'
+import { getImageUrl } from "../../utils";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    
   return (
     <nav className={styles.navbar}>
       <a className={styles.title} href='/'>
       Portfolio
       </a>
       <div className={styles.menu}>
-        <ui className={styles.menuItems}>
+      <img
+          className={styles.menuBtn}
+          src={
+            menuOpen
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
+          alt="menu-button"
+          onClick={() => setMenuOpen(!menuOpen)}
+        />
+        <ul
+        // className={styles.menuItems}
+          className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}
+          onClick={() => setMenuOpen(false)}
+        >
             <li>
                 <a href='about'>About</a>
             </li>
@@ -21,7 +39,7 @@ const Navbar = () => {
             <li>
                 <a href='contact'>Contact</a>
             </li>
-        </ui>
+        </ul>
       </div>
     </nav>
   )
