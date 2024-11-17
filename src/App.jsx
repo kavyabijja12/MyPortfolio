a85740bfe7f7908f9addb109284fd97833bcb10a
kavyabijja12 <bijjakavya1999@gmail.com>
2024-11-16 18:31:11 -0700

    contact added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Hero from './components/Hero/Hero'
 import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
 import { Projects } from './components/Projects/Projects'
+import Contact from './components/Contact/Contact'
+
 function App() {
   return (
     <div className={styles.App}>
@@ -13,6 +15,7 @@ function App() {
     <About/>
     <Experience/>
     <Projects/>
+    <Contact/>
     </div>
   )
 }
