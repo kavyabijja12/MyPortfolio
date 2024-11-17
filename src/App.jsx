b357a6e851ed0e22925631d485258cbf7feb80bc
kavyabijja12 <bijjakavya1999@gmail.com>
2024-11-16 17:33:28 -0700

    projects added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import { About } from './components/About/About'
 import { Experience } from './components/Experience/Experience'
+import { Projects } from './components/Projects/Projects'
 function App() {
   return (
     <div className={styles.App}>
@@ -11,6 +12,7 @@ function App() {
     <Hero/>
     <About/>
     <Experience/>
+    <Projects/>
     </div>
   )
 }
